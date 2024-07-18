bc6adbbdf2451cad35b6aa1b6fe4a0532a79203e
Owen Senowitz <owen.senowitz@gmail.com>
2024-07-18 19:46:05 -0400

    project init frontend and backend

---
+++ instagram-clone-backend/src/main/java/com/example/instagramclonebackend/InstagramCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.instagramclonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramCloneBackendApplication.class, args);
+    }
+
+}
