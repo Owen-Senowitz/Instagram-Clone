d1a26239901ca7b49ac796a80d1bc27f09d30a10
Owen Senowitz <owen.senowitz@gmail.com>
2024-07-18 20:58:25 -0400

    user created

--- instagram-clone-backend/src/main/java/com/example/instagramclonebackend/model/User.java
+++ instagram-clone-backend/src/main/java/com/example/instagramclonebackend/model/User.java
@@ -1,17 +1,22 @@
 package com.example.instagramclonebackend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "users")
 @Getter
 @Setter
 public class User {
     @Id
     private String id;
+
+    @JsonProperty(value = "username")
     private String username;
+
+    @JsonProperty(value = "password")
     private String password;
+
+    @JsonProperty(value = "email")
     private String email;
 }
